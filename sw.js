f1d6f74eac2463db7fd1a1648ee540fd3b770d6c
Mayur Deore <msd7.1993@hotmail.com>
2017-02-27 21:15:50 +0530

    Service Worker added.

---
+++ sw.js
@@ -0,0 +1,25 @@
+﻿
+//declare global cache variables
+var CACHE_NAME = 'test_site_mayur-cache-v1';
+var urlsToCache = [
+    '/'
+    //add files later on 
+];
+
+
+
+
+self.addEventListener('install', function (event) {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(function(cache){
+            console.log('Cache Opened');
+            cache.addAdd(urlsToCache);
+        })
+            
+        );
+   
+
+});
